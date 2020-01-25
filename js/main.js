8052d1d349896a8cd6fca003a8802e30578a57a7
Alex <s1129351@student.windesheim.nl>
2020-01-25 18:11:28 +0100

    Added quicksort function (asc) & more documentation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* GET JSON REQUEST */
 function loadJSONRequest(callback) {
 
     var xobj = new XMLHttpRequest();
@@ -12,44 +13,87 @@ function loadJSONRequest(callback) {
     xobj.send(null);
 }
 
+/* LOAD RESPONSE */
 function getDefaultAfspraken() {
     loadJSONRequest(function (response) {
         // Parse JSON string into object
-        var dataJSON = JSON.parse(response);
-        console.log(dataJSON);
+        const dataJSON = JSON.parse(response);
+        // Sort array
+        selectionSort(dataJSON);
         
-        // DRAW THE HTML TABLE
-        html = "<table class='afsprakenTable'>";
-        html += "<thead>" +
-                "<tr>" +
-                    "<th>Klant naam</th>" +
-                    "<th>Klant adres</th>" +
-                    "<th>Gewenst tijdstip</th>" +
-                    "<th>Dichtsbijzijnde halte</th>" +
-                    "<th>Afstand halte</th>" +
-                    "<th>Reden afspraak</th>" +
-                    "<th>Monteur</th>" +
-                "</tr>" +
-                "</thead>";
-        // Loop through array and add table cells
-        for (var i=0; i<dataJSON.length; i++) {
-            html += "<tr>" + 
-                        "<td>" + dataJSON[i].Afspraak.naamKlant + "</td>" + 
-                        "<td>" + dataJSON[i].Afspraak.adresKlant + "</td>" + 
-                        "<td>" + dataJSON[i].Afspraak.gewenstTijdstip + "</td>" +
-                        "<td>" + dataJSON[i].Afspraak.dichtsbijzijndeHalte + "</td>" +
-                        "<td>" + dataJSON[i].Afspraak.afstandHalte + "</td>" +
-                        "<td>" + dataJSON[i].Afspraak.redenAfspraak + "</td>" +
-                        "<td>" + dataJSON[i].Afspraak.naamMonteur + "</td>" +
-                    "</tr>";
-        }
-        html += "</table>";
-
-        // ATTACH HTML TO DIV
-        document.getElementById("data").innerHTML += html;
+        // /* TEST CONSOLE LOG */
+        // console.log(dataJSON);
+        // setTimeout(function(){ 
+        //     /* SELECTION SORT */
+        //     selectionSort(dataJSON);
+        // }, 3000);
     });
 }
 
+/* SELECTION SORT ARRAY */
+function selectionSort(dataJSON){
+    // Loop through whole array
+    for(var i = 0; i < dataJSON.length; i++){
+        // current index
+        var currentIndex = i;
+        // Loop from the next value of the dataJSONay
+        for(var  j = i + 1; j < dataJSON.length; j++){
+            var nextItem = dataJSON[j].Afspraak.Id;
+            var currentItem = dataJSON[currentIndex].Afspraak.Id;
+            // Check if next item is lower than the current item
+            if(nextItem < currentItem){
+                // If true set current index variable to that item 
+                currentIndex = j;  
+            }
+        }
+        /* Swap values of the dataJSONay based of the check */
+        // Set value of current item as a temporary variable
+        var temporaryFirstItem = dataJSON[i];
+        // Swap current item value with the next item value
+        dataJSON[i] = dataJSON[currentIndex];
+        // Use temporary variable because the current item value has been replaced
+        dataJSON[currentIndex] = temporaryFirstItem;
+    }
+    
+    generateTable(dataJSON);
+}
+
+/* GENERATE HTML TABLE WITH SORTED JSON */
+function generateTable(sortedDataJSON) {
+    // Make basic HTML template with table headers 
+    html = "<table class='afsprakenTable'>";
+    html += "<thead>" +
+            "<tr>" +
+                "<th>ID: </th>" +
+                "<th>Klant naam: </th>" +
+                "<th>Klant adres: </th>" +
+                "<th>Gewenst tijdstip: </th>" +
+                "<th>Dichtsbijzijnde halte: </th>" +
+                "<th>Afstand halte: </th>" +
+                "<th>Reden afspraak: </th>" +
+                "<th>Monteur: </th>" +
+            "</tr>" +
+            "</thead>";
+    // Loop through array and add table cells
+    for (var i=0; i< sortedDataJSON.length; i++) {
+        html += "<tr>" + 
+                    "<td>" + sortedDataJSON[i].Afspraak.Id + "</td>" + 
+                    "<td>" + sortedDataJSON[i].Afspraak.naamKlant + "</td>" + 
+                    "<td>" + sortedDataJSON[i].Afspraak.adresKlant + "</td>" + 
+                    "<td>" + sortedDataJSON[i].Afspraak.gewenstTijdstip + "</td>" +
+                    "<td>" + sortedDataJSON[i].Afspraak.dichtsbijzijndeHalte + "</td>" +
+                    "<td>" + sortedDataJSON[i].Afspraak.afstandHalte + "</td>" +
+                    "<td>" + sortedDataJSON[i].Afspraak.redenAfspraak + "</td>" +
+                    "<td>" + sortedDataJSON[i].Afspraak.naamMonteur + "</td>" +
+                "</tr>";
+    }
+    html += "</table>";
+
+    // Insert HTML table in div
+    document.getElementById("data").innerHTML += html;
+}
+
+/* ASSIGN CLICK LISTENER TO BUTTON */
 const importJSONButton = document.getElementById('importJSON');
 
 importJSONButton.addEventListener('click', event => {
