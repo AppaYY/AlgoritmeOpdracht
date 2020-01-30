37860b68cd734657cfa20023b4d55049837794e5
Alex <s1129351@student.windesheim.nl>
2020-01-30 21:21:09 +0100

    Selection sort on "Gewenst tijdstip" & code optimization

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 function loadJSONRequest(callback) {
 
     var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
+    xobj.overrideMimeType('application/json');
     xobj.open('GET', 'json/defaultAfspraken.json', true);
     xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
+        if (xobj.readyState == 4 && xobj.status == '200') {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             callback(xobj.responseText);
         }
@@ -38,15 +38,15 @@ function selectionSort(dataJSON){
         var currentIndex = i;
         // Loop from the next value of the dataJSONay
         for(var  j = i + 1; j < dataJSON.length; j++){
-            var nextItem = dataJSON[j].Afspraak.Id;
-            var currentItem = dataJSON[currentIndex].Afspraak.Id;
-            // Check if next item is lower than the current item
+            var nextItem = dataJSON[j].Afspraak.gewenstTijdstip;
+            var currentItem = dataJSON[currentIndex].Afspraak.gewenstTijdstip;
+            // Check if next item is greater than the current item
             if(nextItem > currentItem){
                 // If true set current index variable to that item 
                 currentIndex = j;  
             }
         }
-        /* Swap values of the dataJSONay based of the check */
+        /* Swap values of the dataJSON based of the check */
         // Set value of current item as a temporary variable
         var temporaryFirstItem = dataJSON[i];
         // Swap current item value with the next item value
@@ -61,36 +61,34 @@ function selectionSort(dataJSON){
 /* GENERATE HTML TABLE WITH SORTED JSON */
 function generateTable(sortedDataJSON) {
     // Make basic HTML template with table headers 
-    html = "<table class='afsprakenTable'>";
-    html += "<thead>" +
-            "<tr>" +
-                "<th>ID: </th>" +
-                "<th>Klant naam: </th>" +
-                "<th>Klant adres: </th>" +
-                "<th>Gewenst tijdstip: </th>" +
-                "<th>Dichtsbijzijnde halte: </th>" +
-                "<th>Afstand halte: </th>" +
-                "<th>Reden afspraak: </th>" +
-                "<th>Monteur: </th>" +
-            "</tr>" +
-            "</thead>";
+    html = '<table class="afsprakenTable">';
+    html += '<thead>' +
+            '<tr>' +
+                '<th>Klant naam: </th>' +
+                '<th>Klant adres: </th>' +
+                '<th>Gewenst tijdstip: </th>' +
+                '<th>Dichtsbijzijnde halte: </th>' +
+                '<th>Afstand halte: </th>' +
+                '<th>Reden afspraak: </th>' +
+                '<th>Monteur: </th>' +
+            '</tr>' +
+            '</thead>';
     // Loop through array and add table cells
     for (var i=0; i< sortedDataJSON.length; i++) {
-        html += "<tr>" + 
-                    "<td>" + sortedDataJSON[i].Afspraak.Id + "</td>" + 
-                    "<td>" + sortedDataJSON[i].Afspraak.naamKlant + "</td>" + 
-                    "<td>" + sortedDataJSON[i].Afspraak.adresKlant + "</td>" + 
-                    "<td>" + sortedDataJSON[i].Afspraak.gewenstTijdstip + "</td>" +
-                    "<td>" + sortedDataJSON[i].Afspraak.dichtsbijzijndeHalte + "</td>" +
-                    "<td>" + sortedDataJSON[i].Afspraak.afstandHalte + "</td>" +
-                    "<td>" + sortedDataJSON[i].Afspraak.redenAfspraak + "</td>" +
-                    "<td>" + sortedDataJSON[i].Afspraak.naamMonteur + "</td>" +
-                "</tr>";
+        html += '<tr>' + 
+                    '<td>' + sortedDataJSON[i].Afspraak.naamKlant + '</td>' + 
+                    '<td>' + sortedDataJSON[i].Afspraak.adresKlant + '</td>' + 
+                    '<td>' + sortedDataJSON[i].Afspraak.gewenstTijdstip + '</td>' +
+                    '<td>' + sortedDataJSON[i].Afspraak.dichtsbijzijndeHalte + '</td>' +
+                    '<td>' + sortedDataJSON[i].Afspraak.afstandHalte + '</td>' +
+                    '<td>' + sortedDataJSON[i].Afspraak.redenAfspraak + '</td>' +
+                    '<td>' + sortedDataJSON[i].Afspraak.naamMonteur + '</td>' +
+                '</tr>';
     }
-    html += "</table>";
+    html += '</table>';
 
     // Insert HTML table in div
-    document.getElementById("data").innerHTML += html;
+    document.getElementById('data').innerHTML += html;
 }
 
 /* ASSIGN CLICK LISTENER TO BUTTON */
