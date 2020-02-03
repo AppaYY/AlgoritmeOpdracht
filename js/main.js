15c4de43e5f2a60ba08622b68989dd5b1f0c53de
alex pennings <apennings@roc-dev.com>
2020-02-03 11:57:59 +0100

    added example quick sort

--- js/main.js
+++ js/main.js
@@ -130,16 +130,89 @@ const searchButton = document.getElementById('searchMechanicButton');
 searchButton.addEventListener('click', event => {
     appointmentMechanicSearch();
 });
+const sortAppointmentButton = document.getElementById('sortAppointmentButton');
+
+sortAppointmentButton.addEventListener('click', event => {
+    sortAppointments();
+});
 
 function sortAppointments(){
-
-    var a = 97;
-    var charArray = {};
-    for (var i = 0; i<27; i++){
-        charArray[String.fromCharCode(a + i)] = i;
-        if (i == 26){
-            console.log(26);
-            charArray["."] = 0;
+    loadJSONRequest(function (response) {
+        // Parse JSON string into object
+        var dataJSON = JSON.parse(response);
+        console.log(dataJSON);
+        var a = 97;
+        var charArray = {};
+        for (var i = 0; i<27; i++){
+            charArray[String.fromCharCode(a + i)] = i;
+            if (i == 26){
+                console.log(26);
+                charArray["."] = 0;
+            }
         }
-    }
+
+        for (var i=0; i<dataJSON.length; i++){
+            var namevalue = 0;
+            var naam = dataJSON[i].Afspraak.naamKlant;
+
+            for(var y=0; y <naam.length;y++){
+                for (var p=0;p < Object.keys(charArray).length;p++){
+                    if (dataJSON[i].Afspraak.naamKlant.charAt(y) == Object.keys(charArray)[p]){
+                        namevalue = namevalue + charArray[Object.keys(charArray)[p]];
+                        dataJSON[i].Afspraak.namevalue = namevalue;
+                    }
+                }
+            }
+            console.log(dataJSON[i]);
+
+        }
+
+        var items = [5,3,7,6,2,9];
+        function swap(items, leftIndex, rightIndex){
+            var temp = items[leftIndex];
+            items[leftIndex] = items[rightIndex];
+            items[rightIndex] = temp;
+        }
+        function partition(items, left, right) {
+            var pivot   = items[Math.floor((right + left) / 2)], //middle element
+                i       = left, //left pointer
+                j       = right; //right pointer
+            while (i <= j) {
+                while (items[i] < pivot) {
+                    i++;
+                }
+                while (items[j] > pivot) {
+                    j--;
+                }
+                if (i <= j) {
+                    swap(items, i, j); //sawpping two elements
+                    i++;
+                    j--;
+                }
+            }
+            return i;
+        }
+
+        function quickSort(items, left, right) {
+            var index;
+            if (items.length > 1) {
+                index = partition(items, left, right); //index returned from partition
+                if (left < index - 1) { //more elements on the left side of the pivot
+                    quickSort(items, left, index - 1);
+                }
+                if (index < right) { //more elements on the right side of the pivot
+                    quickSort(items, index, right);
+                }
+            }
+            return items;
+        }
+// first call to quick sort
+        var sortedArray = quickSort(items, 0, items.length - 1);
+        console.log(sortedArray); //prints [2,3,5,6,7,9]
+
+
+
+
+
+    });
 }
